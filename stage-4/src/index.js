ba0516d65398e8384f5c15981d9df9aa5a9c5fc9
Matthew Eldredge <mattheweldredge@gmail.com>
2018-01-09 21:22:15 -0700

    stage four

--- stage-4/src/index.js
+++ stage-4/src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { HashRouter } from 'reeact-router-dom'
 
 ReactDOM.render(
-
-    <Provider store={store}>
-        <App />
-    </Provider>
+    <HashRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </HashRouter>
     , document.getElementById('root'));
 registerServiceWorker();
--- stage-4/src/redux/store.js
+++ stage-4/src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducer';
 import reduxPromiseMiddleware from 'redux-promise-middleware';
+import redux from 'react-redux'
 
 export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
